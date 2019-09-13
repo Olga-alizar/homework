4e60f9603c228a1530b765a7d734dba214267c65
Olga-alizar <54671370+Olga-alizar@users.noreply.github.com>
2019-09-13 15:47:35 +0300

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,8 @@
+//Задание 1
+//Набрать приведенный пример, откомпилировать его и запустить.
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
