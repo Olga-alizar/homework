bf1f61a29a8865cfdb9847565bd1a26402b042be
Olga-alizar <54671370+Olga-alizar@users.noreply.github.com>
2019-09-13 19:48:29 +0300

    Add files via upload

--- TimePrinter.java
+++ TimePrinter.java
@@ -1,15 +1,23 @@
+//Имеется 4500 секунд. Вывести в консоль содержащихся в этом количестве секунд:
+//        А) минут + секунд,
+//        В) часов + минут + секунд,
+//        С) дней + часов + минут + секунд,
+//        D) недель + дней + часов + минут + секунд.
+//        по аналогии с примером выше.
+
+
 public class TimePrinter {
-public static void main(String[] args) {
-int s = 4500;
-int sec = s % 60;
-int m = (s - sec) / 60;
-int min = m % 60;
-int h = (m - min) / 60;
-int d = s / 86400;
-int n = s / 604800;
-System.out.println(min + " минут " + sec + " секунд ");
-System.out.println(h + " часов " + min + " минут " + sec + " секунд ");
-System.out.println(d + " дней " + h + " часов " + min + " минут " + sec + " секунд ");
-System.out.println(n + " недель " + d + " дней " + h + " часов " + min + " минут " + sec + " секунд");
-}
+    public static void main(String[] args) {
+        int s = 4500;
+        int sec = s % 60;
+        int m = (s - sec) / 60;
+        int min = m % 60;
+        int h = (m - min) / 60;
+        int d = s / 86400;
+        int n = s / 604800;
+        System.out.println(min + " минут " + sec + " секунд");
+        System.out.println(h + " часов " + min + " минут " + sec + " секунд");
+        System.out.println(d + " дней " + h + " часов " + min + " минут " + sec + " секунд");
+        System.out.println(n + " недель " + d + " дней " + h + " часов " + min + " минут " + sec + " секунд");
+    }
 }
